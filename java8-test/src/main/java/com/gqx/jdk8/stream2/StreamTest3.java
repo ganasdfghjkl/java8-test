f997b5a7e57a4f3269c0b8fc218625a914c31532
jegqx <ganqixin>
2020-10-13 16:41:20 +0800

    ....

--- java8-test/src/main/java/com/gqx/jdk8/stream2/StreamTest3.java
+++ java8-test/src/main/java/com/gqx/jdk8/stream2/StreamTest3.java
@@ -11,14 +11,14 @@ import java.util.stream.Stream;
  * @author gqx
  * @date 2020/8/25 8:56
  */
-public class StreamTest3 {
+public  class StreamTest3 {
 
     public static void main(String[] args) {
         List<String> list = Arrays.asList("hello","world","hello world"
         ,"teacher","welcome","person","student"
         );
     ///        list.stream().forEach(System.out::println);
-        list.stream().distinct().filter(a->a.length()>5).map(a->a+"_abc").findAny();
+        list.stream().distinct().filter(a->a.length()>5).map(a->a+"_abc").forEach(System.out::println);
 
     }
 }
